3b9672b75100959eb189e7fcb6a65646f74d8cac
TaXueGeXing <154244678+TaXueGeXing@users.noreply.github.com>
2024-08-24 05:38:25 +0100

    Update create_new.sh

--- create_new.sh
+++ create_new.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
-# 更新软件包索引
 sudo apt update
-
-# 升级所有已安装的软件包
 sudo apt upgrade -y
 
-# 安装 sysbench
 sudo apt install -y sysbench
-
-# 安装 git
 sudo apt install -y git
 
-# 安装 Python 3 和 pip
 sudo apt install -y python3 python3-pip
-
-# 安装默认的 JDK
 sudo apt install -y default-jdk
+sudo apt install -y zip
 
-# 下载并安装 Nextflow
 curl -s https://get.nextflow.io | bash
 sudo mv nextflow /usr/local/bin/
